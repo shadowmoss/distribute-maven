666371d6ee0c5268018e7026b3ce31f75c6352c3
GFKJ-XXH-LUTX\asus <1577634892@qq.com>
2024-03-07 16:13:26 +0800

    sentinel流量管控组件

--- cloudalibaba-sentinel-service8401/src/main/java/com/ocean/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/ocean/controller/FlowLimitController.java
@@ -5,6 +5,8 @@ import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class FlowLimitController {
     @GetMapping("/testA")
@@ -31,9 +33,48 @@ public class FlowLimitController {
         return "------testD";
     }
 
+    /**
+     * 测试流量效果的排队等待效果
+     * @return
+     */
     @GetMapping("/testE")
     public String testE(){
         System.out.println(System.currentTimeMillis()+"      testE,排队等待");
         return "------testE";
+    }
+
+    /**
+     * 熔断策略测试:慢调用比例测试
+     */
+    @GetMapping("/testF")
+    public String testF(){
+        try{
+            TimeUnit.SECONDS.sleep(1);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "------testF 新增熔断测试-慢调用比例";
+    }
+
+    /**
+     * 熔断规则-异常比例
+     * @return
+     */
+    @GetMapping("/testG")
+    public String testG(){
+        System.out.println("------测试:新增熔断规则-异常比例");
+        int age = 10/0;
+        return "------testG,新增熔断规则-异常比例";
+    }
+
+    /**
+     * 熔断规则-异常数
+     * @return
+     */
+    @GetMapping("/testH")
+    public String testH(){
+        System.out.println("------测试:新增熔断规则-异常比例");
+        int age = 10/0;
+        return "------testG,新增熔断规则-异常比例";
     }
 }
